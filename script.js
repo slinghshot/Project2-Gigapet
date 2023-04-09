4df54ff21d38ea3eba78428bd74a86a461eca31a
slinghshot <simerpreet.singh40@gmail.com>
2023-04-09 16:25:24 -0700

    added heal, and pet comments

--- script.js
+++ script.js
@@ -11,15 +11,20 @@ $(function () {
 });
 
 // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
-var pet_info = { name: "Baulbasaur", weight: 15, happiness: 5, health: 45 };
+var pet_info = { name: "Bulbasaur", weight: 35, happiness: 5, health: 45, 
+    healLines: ["heal feel good","I'm feeling better","Bulbasaur!"],
+    treatLines:["Treats make me happy","more treats please","Bulbasaur"],
+    exerciseLines: ["Great workout", "I'm exhausted", "no more exercise"],
+    playLines:["lets keep going!", "More play time!", "I'm getting tired"] };
 
 function clickedTreatButton() {
   // Increase pet happiness
   pet_info.happiness += 1;
   // Increase pet weight
-  pet_info.weight += 1;
+  pet_info.weight += 0.5;
   // Increase pet health
-  clickedHealButton(5);
+  clickedHealButton(1);
+  updateMessage(pet_info.treatLines);
   checkAndUpdatePetInfoInHtml();
 }
 
@@ -27,32 +32,42 @@ function clickedPlayButton() {
   // Increase pet happiness
   pet_info.weight === 0 ? console.log("feed me") : (pet_info.happiness += 2);
   // Decrease pet weight
-  pet_info.weight -= 2;
+  pet_info.weight -= 1;
   clickedHealButton(-5);
+  updateMessage(pet_info.playLines);
   checkAndUpdatePetInfoInHtml();
 }
 
 function clickedExerciseButton() {
   // Decrease pet happiness
-  pet_info.happiness -= 2;
+  pet_info.happiness -= 1.5;
   // Decrease pet weight
   pet_info.weight -= 3;
-  clickedHealButton(5);
+  updateMessage(pet_info.exerciseLines);
   checkAndUpdatePetInfoInHtml();
 }
 
 function clickedHealButton(healAmount) {
   const maxHealth = 100;
   const minHealth = 0;
-  console.log(pet_info.health, healAmount);
+  // console.log(pet_info.health, healAmount);
   pet_info.health += healAmount;
   if (pet_info.health < minHealth) {
     pet_info.health = minHealth;
   } else if (pet_info.health > maxHealth) {
     pet_info.health = maxHealth;
   }
-
+  if(pet_info.health===100){
+    pet_info.weight=35;
+    pet_info.happiness=10;
+  }
+  updateMessage(pet_info.healLines);
   checkAndUpdatePetInfoInHtml();
+}
+function updateMessage(message){
+  let x = Math.floor(Math.random()*message.length);
+  document.getElementById('msg').innerHTML=message[x];
+
 }
 
 function checkAndUpdatePetInfoInHtml() {
@@ -62,12 +77,12 @@ function checkAndUpdatePetInfoInHtml() {
 // if weight <2 then happiness is set to 2, 1 then 1 0 then 0 dead
 function checkWeightAndHappinessBeforeUpdating() {
   // Add conditional so if weight is lower than zero.
-  if (pet_info.weight <= 2) {
+  if (pet_info.weight <= 10) {
     pet_info.happiness =
-      pet_info.happiness > 2 ? pet_info.weight : pet_info.happiness;
-    updatePetImg(".c");
+      pet_info.happiness > 10 ? pet_info.weight : pet_info.happiness;
+    updatePetImg(".sad");
   } else if (pet_info.weight > 2) {
-    updatePetImg(".a");
+    updatePetImg(".neutral");
   }
 
   pet_info.weight < 0 ? (pet_info.weight = 0) : pet_info.weight;
@@ -76,7 +91,7 @@ function checkWeightAndHappinessBeforeUpdating() {
     pet_info.happiness = 0;
   } else if (pet_info.happiness >= 10) {
     pet_info.happiness = 10;
-    updatePetImg(".b");
+    updatePetImg(".awesome");
   }
 }
 
@@ -86,10 +101,22 @@ function updatePetInfoInHtml() {
   $(".weight").text(pet_info["weight"]);
   $(".happiness").text(pet_info["happiness"]);
   $(".health").text(pet_info["health"]);
+  
+  if(pet_info.health===0){
+    document.getElementById('msg').innerHTML="heal me first, please!";
+  }
+  $(".treat-button").prop("disabled",pet_info.health>0? false: true);
+  $(".play-button").prop("disabled",pet_info.health>0? false: true);
+  $(".exercise-button").prop("disabled",pet_info.health>0? false: true);
 }
-function updatePetImg(a) {
-  $(".c").hide();
-  $(".b").hide();
-  $(".a").hide();
-  $(a).show();
+function updatePetImg(activate) {
+  // My two methods are hide() and show().
+  // Based on weight and happiness, 
+  // if pet is less than 11 weight its sad image
+  // if pet is 10 happiness its happy image
+  // if pet is above 10 weight and below 10 happiness its neutral image.
+  $(".sad").hide();
+  $(".awesome").hide();
+  $(".neutral").hide();
+  $(activate).show();
 }
