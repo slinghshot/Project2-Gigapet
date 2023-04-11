4b689403a12dfddd67b0b98de87821b0c299cd66
slinghshot <simerpreet.singh40@gmail.com>
2023-04-10 20:41:26 -0700

    Added script with no jQuery

--- scriptNoQ.js
+++ scriptNoQ.js
@@ -1,13 +1,13 @@
-$(function () {
+document.addEventListener("DOMContentLoaded", function() {
   // Makes sure that your function is called once all the DOM elements of the page are ready to be used.
   // Called function to update the name, happiness, and weight of our pet in our HTML
   checkAndUpdatePetInfoInHtml();
 
   // When each button is clicked, it will "call" function for that button (functions are below)
-  $(".treat-button").click(clickedTreatButton);
-  $(".play-button").click(clickedPlayButton);
-  $(".exercise-button").click(clickedExerciseButton);
-  $(".heal-button").click(() => clickedHealButton(10));
+  document.querySelector(".treat-button").addEventListener("click", clickedTreatButton);
+  document.querySelector(".play-button").addEventListener("click", clickedPlayButton);
+  document.querySelector(".exercise-button").addEventListener("click", clickedExerciseButton);
+  document.querySelector(".heal-button").addEventListener("click", function() { clickedHealButton(10); });
 });
 
 // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
@@ -62,7 +62,7 @@ function clickedHealButton(healAmount) {
     pet_info.health = MAXHEALTH;
   }
   if(pet_info.health===100){
-    pet_info.weight=35;
+    pet_info.weight+=25;
     pet_info.happiness=10;
   }
   updateMessage(pet_info.healLines);
@@ -71,7 +71,6 @@ function clickedHealButton(healAmount) {
 function updateMessage(message){
   let x = Math.floor(Math.random()*message.length);
   document.getElementById('msg').innerHTML=message[x];
-
 }
 
 function checkAndUpdatePetInfoInHtml() {
@@ -80,44 +79,45 @@ function checkAndUpdatePetInfoInHtml() {
 }
 
 function checkWeightAndHappinessBeforeUpdating() {
-  // Add conditional so if weight is lower than zero.
+  
   if (pet_info.happiness <= 4) {
-    updatePetImg(".sad");
-  } else if (pet_info.weight > 4) {
-    updatePetImg(".neutral");
+    updatePetImg("sad");
+  } else if (pet_info.happiness > 4) {
+    updatePetImg("neutral");
   }
 
-  pet_info.weight < 0 ? (pet_info.weight = 0) : pet_info.weight;
+  pet_info.weight = (pet_info.weight < 0) ? 0 : pet_info.weight;
 
   if (pet_info.happiness < 0) {
     pet_info.happiness = 0;
   } else if (pet_info.happiness >= 10) {
     pet_info.happiness = 10;
-    updatePetImg(".awesome");
+    updatePetImg("awesome");
   }
 }
 
 // Updates your HTML with the current values in your pet_info object
 function updatePetInfoInHtml() {
-  $(".name").text(pet_info["name"]);
-  $(".weight").text(pet_info["weight"]);
-  $(".happiness").text(pet_info["happiness"]);
-  $(".health").text(pet_info["health"]);
-  
-  if(pet_info.health===0){
-    document.getElementById('msg').innerHTML="heal me first, please!";
+  document.querySelector(".name").textContent = pet_info["name"];
+  document.querySelector(".weight").textContent = pet_info["weight"];
+  document.querySelector(".happiness").textContent = pet_info["happiness"];
+  document.querySelector(".health").textContent = pet_info["health"];
+
+  if (pet_info.health === 0) {
+    document.getElementById('msg').innerHTML = "heal me first, please!";
   }
-  $(".treat-button").prop("disabled",pet_info.health>0? false: true);
-  $(".play-button").prop("disabled",pet_info.health>0? false: true);
-  $(".exercise-button").prop("disabled",pet_info.health>0? false: true);
+  document.querySelector(".treat-button").disabled = (pet_info.health > 0) ? false : true;
+  document.querySelector(".play-button").disabled = (pet_info.health > 0) ? false : true;
+  document.querySelector(".exercise-button").disabled = (pet_info.health > 0) ? false : true;
 }
+
 function updatePetImg(activate) {
   // My two methods are hide() and show().
   // if pet is 10 happiness its happy image
   // if pet is less than or equal 4 then sad image
   // if pet is above 4 weight and below 10 happiness its neutral image.
-  $(".sad").hide();
-  $(".awesome").hide();
-  $(".neutral").hide();
-  $(activate).show();
+  document.querySelector(".sad").style.display = "none";
+  document.querySelector(".awesome").style.display = "none";
+  document.querySelector(".neutral").style.display = "none";
+  document.querySelector("." + activate).style.display = "block";
 }
