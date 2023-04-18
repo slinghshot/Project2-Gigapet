36ce217e51a71ea47d4a21a69ba29ac3ca878ea9
Slinghshot <simerpreet.singh40@gmail.com>
2023-04-17 18:20:29 -0700

    Added comments

--- script.js
+++ script.js
@@ -11,12 +11,15 @@ $(function () {
 });
 
 // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
+// This includes arrays of healLines, treatLines, exerciseLines and playLines.
 var pet_info = { name: "Bulbasaur", weight: 35, happiness: 5, health: 45, 
     healLines: ["heal feel good","I'm feeling better","Bulbasaur!"],
     treatLines:["Treats make me happy","more treats please","Bulbasaur"],
     exerciseLines: ["Great workout", "I'm exhausted", "no more exercise"],
     playLines:["lets keep going!", "More play time!", "I'm getting tired"] };
 
+// when treat-button is clicked, this function gets called.
+// addes 1 to happiness and 0.5 to weight. then calls clickedHealButton, updateMessage and checkAndUpdatePetInfoHtml
 function clickedTreatButton() {
   // Increase pet happiness
   pet_info.happiness += 1;
@@ -27,7 +30,8 @@ function clickedTreatButton() {
   updateMessage(pet_info.treatLines);
   checkAndUpdatePetInfoInHtml();
 }
-
+// when play button is clicked this functions is called
+// checks if weight is 0 then alerts user to feed it first. 
 function clickedPlayButton() {
   // Increase pet happiness
   pet_info.weight === 0 ? alert("feed me first!"): (pet_info.happiness += 2);
@@ -38,6 +42,7 @@ function clickedPlayButton() {
   checkAndUpdatePetInfoInHtml();
 }
 
+// when exercise button is clicked, check if weight is 0, if it is alert and return else decrease happiness and weight
 function clickedExerciseButton() {
   if(pet_info.weight===0){
     alert('Feed Me First!');
@@ -50,7 +55,10 @@ function clickedExerciseButton() {
   updateMessage(pet_info.exerciseLines);
   checkAndUpdatePetInfoInHtml();
 }
-
+// heal button clicked this function is called, with healAmount argument.
+// set max and min health, then healAmount gets added to health.
+// if pet health is less than minHealth then set it to MinHealth
+// if pet health is greater than maxHealth then set it to maxhealth
 function clickedHealButton(healAmount) {
   const MAXHEALTH = 100;
   const MINHEALTH = 0;
@@ -68,26 +76,34 @@ function clickedHealButton(healAmount) {
   updateMessage(pet_info.healLines);
   checkAndUpdatePetInfoInHtml();
 }
+
+// updateMessage, randomly picks a message from the array that is passed and then set is.
 function updateMessage(message){
   let x = Math.floor(Math.random()*message.length);
   document.getElementById('msg').innerHTML=message[x];
 
 }
 
+// checkANdUPdatePetInfoInHtml sets 
 function checkAndUpdatePetInfoInHtml() {
   checkWeightAndHappinessBeforeUpdating();
   updatePetInfoInHtml();
 }
 
+// checkWeightANdHappinessBeforeUpdating function checks happiness and based on it updates pet Image.
+// if happiness is less than 5 set image to sad
+// if happiness is less greater than 4 set image to neutral
+// if happiness = 10 set awesome image
 function checkWeightAndHappinessBeforeUpdating() {
   // Add conditional so if weight is lower than zero.
   if (pet_info.happiness <= 4) {
     updatePetImg(".sad");
-  } else if (pet_info.weight > 4) {
+  }
+   else if (pet_info.happiness > 4) {
     updatePetImg(".neutral");
   }
 
-  pet_info.weight < 0 ? (pet_info.weight = 0) : pet_info.weight;
+  // pet_info.weight < 0 ? (pet_info.weight = 0) : pet_info.weight;
 
   if (pet_info.happiness < 0) {
     pet_info.happiness = 0;
@@ -111,6 +127,7 @@ function updatePetInfoInHtml() {
   $(".play-button").prop("disabled",pet_info.health>0? false: true);
   $(".exercise-button").prop("disabled",pet_info.health>0? false: true);
 }
+// hides all images and then shows class image that is passed.
 function updatePetImg(activate) {
   // My two methods are hide() and show().
   // if pet is 10 happiness its happy image
